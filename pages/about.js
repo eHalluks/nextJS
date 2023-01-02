faffc9fa7f7cd239eca8c98701ff9851bf1d9763
eHalluks <info@ehalluks.fun>
2023-01-02 20:51:02 +0100

    start commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return(
+        <div>
+            <h1>Hello from about page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A commodi culpa ex itaque iure, voluptatem voluptates? Animi architecto atque cum eligendi, illum molestias neque perferendis perspiciatis soluta ullam! Dolores, earum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium fuga ipsum? Animi dignissimos distinctio dolor dolorum et ex excepturi exercitationem, incidunt, ipsum laudantium magni nobis odit officia provident, quisquam.</p>
+        </div>
+    )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function AddPage() {
+    return(
+        <div>
+            <h1>Hello from add event page</h1>
+        </div>
+    )
+}
